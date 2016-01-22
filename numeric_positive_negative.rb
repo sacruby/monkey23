02768a590f851bded7cf5ba809883d143cab658d
Thomas Hopkins <thomas@fullstacklabs.co>
2016-01-21 18:58:36 -0800

    Add test for negative?

--- numeric_positive_negative.rb
+++ numeric_positive_negative.rb
@@ -5,9 +5,17 @@ class Numeric
 end
 
 describe Numeric do
-  describe "#positive" do
-    it "returns true if a nubmer is positive" do
+  describe "#positive?" do
+    it "returns true if a number is positive" do
       4.monkey_positive?.must_equal true
+      0.monkey_positive?.must_equal false
+    end
+  end
+
+  describe "#negative?" do
+    it "returns true if a number is negative" do
+      -4.monkey_negative?.must_equal true
+      -0.monkey_negative?.must_equal false
     end
   end
 end
